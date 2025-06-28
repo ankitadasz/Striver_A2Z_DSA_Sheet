5a51c06437928bde8f3c6c85aa610a9985cdd72a
Ankita Das <daszankita23@gmail.com>
2025-06-28 22:20:04 +0530

    patterns

---
+++ Patterns/tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n=3;
+    for(i=0;i<2*n-1;i++){
+        for(j=0;j<2*n-1;j++){
+            if(i==0||j==0||i==2*n-1||j==2*n-1){
+                cout<<n;
+            }
+            else if(i==n-1&&j==n-1){
+                cout<<"1";
+            }
+            else{
+                cout<<n-1;
+            }
+        }
+        cout<<endl;
+    }
+}
