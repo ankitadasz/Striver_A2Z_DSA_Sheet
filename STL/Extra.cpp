740b9a1e35d8a03f769b8a04903875f62c912191
Ankita Das <daszankita23@gmail.com>
2025-07-02 21:09:42 +0530

    Extra

---
+++ STL/Extra.cpp
@@ -0,0 +1,7 @@
+//sort(a,a+n) sort in ascending order
+//sort(a,a+n,greater<int>)sort in descending order
+//sort(a,a+n,comp)
+/*int num=6
+__builtin_popcount() it will count the no of 1 in binary format of 6
+for long long add __builtin_popcountll()
+*/
