7ba3d0bdbf07f88bac71a58ff07530378698d379
Ankita Das <daszankita23@gmail.com>
2025-07-02 22:37:06 +0530

    Reverse Number

--- BasicMaths/ReverseNumber.cpp
+++ BasicMaths/ReverseNumber.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n=1,cnt=0;
+    int n=1234,num=0;
     while(n!=0){
+        num=n%10;
         n=n/10;
-        cnt++;
-
+        cout<<num;
     }
-    cout<<cnt;
+    
 }
