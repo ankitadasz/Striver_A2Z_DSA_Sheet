9017970279165a73c59bcd51e81e10ae48a6d009
Ankita Das <daszankita23@gmail.com>
2026-02-01 20:56:14 +0530

    GCD or HCF

--- BasicMaths/GCD.cpp
+++ BasicMaths/GCD.cpp
@@ -5,7 +5,7 @@ int main(){
   cout<<"Enter n1 and n2:";
   cin>>n1>>n2; 
      cout<<"GCD is:";
-  for(i=1;i<=min(n1,n2);i++){
+  for(i=1;i<=min(n1,n2);i++){//here we are taking minimum because the factor cant exceed the minimum number between two num
 
     if(n1%i==0&&n2%i==0){
         cout<<i<<" ";
