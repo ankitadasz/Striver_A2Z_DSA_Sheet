91460e47c76f068d48bcea92435dd740bf9c9b66
Ankita Das <daszankita23@gmail.com>
2026-02-01 21:19:55 +0530

    Print All Divisors

--- BasicMaths/Divisor.cpp
+++ BasicMaths/Divisor.cpp
@@ -1,11 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n=36,i;
+    int n,i;
+    cout<<"Enter the value of n: ";
+    cin>>n;
     cout<<"divisors are:";
     for(i=1;i<=n;i++){
         if(n%i==0){
             cout<<i<<" ";
         }
     }
+    
 }
