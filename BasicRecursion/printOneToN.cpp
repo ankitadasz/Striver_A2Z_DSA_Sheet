4b0fd5ff918945dd06c795d31a470c89a558902f
Ankita Das <daszankita23@gmail.com>
2026-02-02 21:38:41 +0530

    print 1 to n using recursion

--- BasicRecursion/printOneToN.cpp
+++ BasicRecursion/printOneToN.cpp
@@ -1,15 +1,16 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-void printNum(int i, int n){
-    cout<<"Enter the value of n:";
-    cin>>n;
-    while(i<=n){
-        cout<<i<<" ";
-        i++;
-    }
-    
+void printNum(int i,int n){
+    if(i>n)
+    return;
+    cout<<i<<" ";
+    printNum(i+1,n);
+
 }
 int main(){
     int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
     printNum(1,n);
+
 }
