9f455d745066b3a8c21f36caeaf53192454c1c2f
Ankita Das <daszankita23@gmail.com>
2026-02-02 21:40:07 +0530

    Print Name n times using recursion

--- BasicRecursion/PrintName.cpp
+++ BasicRecursion/PrintName.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 void printName(string name){
-    cout<<"Enter your name:";
-    cin>>name;
     cout<<"Hey "<<name<<":)";
 }
 int main(){
     string n;
+    cout<<"Enter your name:";
+    cin>>n;
     printName(n);
 }
