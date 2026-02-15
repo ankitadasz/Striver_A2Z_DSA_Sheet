cc073583f335799c9ab8055ab6724212ed37ab2c
Ankita Das <daszankita23@gmail.com>
2026-02-15 13:07:39 +0530

    Fibonacci number

--- BasicRecursion/Fibonacchi.cpp
+++ BasicRecursion/Fibonacchi.cpp
@@ -1,14 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int Fibonacchi(int n){
-    if(n==0) return 0;
-    if(n==1) return 1;
-    
-    return Fibonacchi(n-1)+Fibonacchi(n-2);
+int Fibonacchi(int n)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
 
+    return  Fibonacchi(n - 1)+Fibonacchi(n - 2);
 }
-int main(){
-     int n=7;
-     int ans=Fibonacchi(n);
-     cout<<ans;
+int main()
+{
+    int n;
+    cout << "Enter the value of n:";
+    cin >> n;
+    cout << "Fibonacchi series is:";
+    for(int i=0;i<n;i++){
+        cout<<Fibonacchi(i)<<" ";
+    }
+    
+    
 }
--- BasicRecursion/tempCodeRunnerFile.cpp
+++ BasicRecursion/tempCodeRunnerFile.cpp
@@ -1,18 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int Factorial(int n){
-    if(n==1)
-    return 1;
-    if(n==0){
+int Fibonacchi(int n)
+{
+    if (n <= 0)
+    {
         return 0;
     }
+    else if (n == 1)
+    {
+        return 1;
+    }
 
-    return(n*Factorial(n-1));
-
+    return  Fibonacchi(n - 1)+Fibonacchi(n - 2);
 }
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the value of n:";
-    cin>>n;
-    cout<<"Factorial of "<<n<<" is: "<<Factorial(n);
+    cout << "Enter the value of n:";
+    cin >> n;
+    cout << "Fibonacchi series is:";
+    for(int i=0;i<n;i++){
+        cout<<Fibonacchi(i);
+    }
+    
+    
 }
