71c9858504c2b402812b353ea0f105d305507d03
Ankita Das <daszankita23@gmail.com>
2026-03-18 20:05:25 +0530

    Character Hashing

---
+++ Hashing/CharacterHashing.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"Enter the string:";
+    cin >> s;
+    int hash[26]= {0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]-'a']++;
+    }
+    int q;
+    cout<<"Enter how many letter you want to check:";
+    cin>>q;
+     cout<<"Enter the character you want to check for:";
+    while(q--){
+        char c;
+        cin>>c;
+        cout<<c<<" : "<< hash[c-'a']<<endl;
+    }
+}
