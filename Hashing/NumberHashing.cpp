eb466b69a61dd20cca15284179f486ad42d8064f
Ankita Das <daszankita23@gmail.com>
2026-03-18 23:17:07 +0530

    Number Hashing using MAP

--- Hashing/NumberHashing.cpp
+++ Hashing/NumberHashing.cpp
@@ -1,25 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the no of array element:";
-    cin>>n;
-     int arr[n];
-     cout<<"Enter the array elements:";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    map<int,int>mpp;
-    for(int i=0;i<n;i++){
+    cout << "Enter the no of array element:";
+    cin >> n;
+    int arr[n];
+    map<int, int> mpp;
+    cout << "Enter the array elements:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
         mpp[arr[i]]++;
     }
+
+    for (auto it : mpp)
+    {
+        cout << it.first << "->" << it.second << endl;
+    }
     int q;
-    cout<<"Enter how many numbers you want to search:";
-    cin>>q;
-    cout<<"Enter the numbers you want to search:";
-    while(q--){
+    cout << "Enter how many numbers you want to search:";
+    cin >> q;
+    cout << "Enter the numbers you want to search:";
+    while (q--)
+    {
         int num;
-        cin>>num;
-        cout<<num<<" : "<<mpp[num]<<endl;
+        cin >> num;
+        cout << num << " : " << mpp[num] << endl;
     }
 }
