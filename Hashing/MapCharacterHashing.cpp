068e8a3d3a57f2adc4f1ffbc4419f3f0df9a22e8
Ankita Das <daszankita23@gmail.com>
2026-03-19 08:19:21 +0530

    character Hashing using MAP

---
+++ Hashing/MapCharacterHashing.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"ENter the number of array elements:";
+    cin>>n;
+    char arr[n];
+    cout<<"Enter the elements of the array:";
+    map <char,int>mpp;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        mpp[arr[i]]++;
+    }
+    int q;
+    cout<<"Enter how many letters you want to check:";
+    cin>>q;
+    cout<<"Enter the character you want to check for:";
+    while(q--){
+        char c;
+        cin>>c;
+        cout<<c<<" : "<<mpp[c]<<endl;
+    }
+}
